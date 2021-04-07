e2ae392be34667d915343b9ee4c0c7fab4b532c2
superlinkzelda <jerry.cewu@gmail.com>
2021-04-07 22:44:41 +0800

    I added the random number function as expotential, but I don't know what x represents in the text

--- main.c
+++ main.c
@@ -15,9 +15,8 @@ void arrive(int* server_status, int* num_in_queue, int* num_custs_delayed,
 	float* next_arr_time, float *next_dept_time);
 
 
-float f(float mean, float t){
-	return ((1/mean)*exp(-t/mean));
-};
+float expon(float Mean);
+
 
 int main()
 {
@@ -37,43 +36,40 @@ int main()
     float next_dept_time=pow(10,30);
 
 	float mean_interarrival_time = 0.0; //initialize input
-    	float mean_service_time = 0.0;
+    float mean_service_time = 0.0;
+
+
 
 	printf("Enter mean_interarrival_time\n");
 	scanf("%f", &mean_interarrival_time);
+
+
+
 	printf("Enter mean_service_time\n");
 	scanf("%f", &mean_service_time);
-	
-  float next_arr_time=expon(mean_interarrival_time);	
-    
+
+  float next_arr_time=expon(mean_interarrival_time,x);
+
     for (int i=0;i<Q_limit;i++){ // initialize time_arrival
         time_arrival[i]=0.0;
     }
 
- /*   printf("sim_time= %d \n",sim_time);         // testing initialized value
-    printf("server_status= %d \n",server_status);
-    printf("num_in_queue= %d \n",num_in_queue);
-    printf("num_custs_delayed= %d \n",num_custs_delayed);
-    printf("num_delays_required= %d \n",num_delays_required);
-    printf("total_delay= %.4f \n",total_delay);
-    printf("avg_delay= %.4f \n",avg_delay);
-    printf("avg_num_in_queue= %.4f \n",avg_num_in_queue);
-    printf("area_under_Q= %.4f \n",area_under_Q);
-    printf("last_event_time= %.4f \n",last_event_time);
-    printf("time_since_last_event= %.4f \n",time_since_last_event);
-*/
+
+
+
+
     while(num_custs_delayed<num_delays_required){
 
         if(next_arr_time<next_dept_time){
-            
+
             arrive(&server_status,&num_in_queue,&num_custs_delayed,&sim_time,&area_under_Q,&last_event_time,&time_since_last_event,
 		&time_arrival[Q_limit],&mean_interarrival_time,&mean_service_time,&next_arr_time,&next_dept_time);
         }
         else{
-            
+
             depart();
-        }   
-        
+        }
+
     }
     printf("\n");
     printf("!!!");//report;
@@ -88,9 +84,9 @@ int main()
     printf("area_under_Q= %.4f \n",area_under_Q);
     printf("last_event_time= %.4f \n",last_event_time);
     printf("time_since_last_event= %.4f \n",time_since_last_event);
-    
+
     return 0;
-    
+
 }
 
 void arrive(int* server_status,int* num_in_queue,int* num_custs_delayed,
@@ -98,16 +94,16 @@ float* sim_time,float* area_under_Q,float* last_event_time,float* time_since_las
 float* time_arrival[Q_limit],float* mean_interarrival_time,float* mean_service_time,
 float* next_arr_time,float *next_dept_time) // change to call by reference
 
-{	
+{
 	*sim_time = *next_arr_time;
 	*next_arr_time = *sim_time + expon(*mean_interarrival_time);
 
-	
+
 	*time_since_last_event = *sim_time - *last_event_time;
 	*last_event_time = *sim_time;
 	*area_under_Q += *num_in_queue * *time_since_last_event;
-	
-	
+
+
 	if(*server_status == BUSY)
 	{
 		++ *num_in_queue;
@@ -121,61 +117,56 @@ float* next_arr_time,float *next_dept_time) // change to call by reference
 	}
 	else
 	{
-		
+
 		++*num_custs_delayed;
-		*server_status = BUSY;	
-		
+		*server_status = BUSY;
+
 		*next_dept_time = *sim_time + expon(*mean_service_time);
 	}
 }
 
 void depart(void)  // change to call by regerence
 
-{	
+{
     sim_time = next_dept_time;
 
 	time_since_last_event = sim_time - last_event_time;
 	last_event_time = sim_time;
 	area_under_Q += num_in_queue * time_since_last_event;
-	
-	
+
+
 	//Check to wheather the queue is empty
-	
+
 	if(num_in_queue == 0)
 	{
 		server_status = IDLE;
 		next_dept_time = pow(10,30);
 	}
-	
+
 	else
 	{
 		--num_in_queue;
 		Q_delay = sim_time-time_arrival[1];
-		
+
 		total_q_delay += q_delay;
-		
+
 		++num_custs_delayed;
-		
+
 		next_dept_time = sim_time+expon(mean_service_time);
-		
+
 		//Move each customer in queue (if any) up one place
 		for(int i=1;i<=num_in_queue;++i)
-		
+
 		time_arrival[i] = time_arrival[i+1];
 	}
 }
 
-	float expon(lower_bound=0.0,upper_bound, width, t,sum=0.0,integral); // not sure aboud upper_bound value 
-	int n; // number of sub intervals;
-	width=(upper_bound-lower_bound)/n;
-	int i;
-	for (i=0;,i<n;i++){
-		t=lower_bound+i*width;
-	}
-	
-	sum=sum+f(mean , t);
-	integral=h/2.0*(f(lower_bound)+2*sum+f(upper_bound));
-	
-	
-	return integral; //這個1是說MEAN=1，這要改成我打的INPUT
+float expon(float Mean){
+
+   float integral=0.0 , float x=1.0;
+
+   integral=1-exp(-x/Mean);
+
+
+	return integral;
 }
